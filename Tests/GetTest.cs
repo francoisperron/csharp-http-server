50d752fa69f5441a46f0b14a8391c00c4167d369
Francois Perron <fperron@gmail.com>
2015-09-03 21:18:44 -0400

    with GET test

---
+++ Tests/GetTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using NUnit.Framework;
+using RestSharp;
+
+namespace Tests
+{
+    [TestFixture]
+    class GetTest
+    {
+        [Test]
+        public void GetReturnsOk()
+        {
+            var client = new RestClient("localhost:8080");
+            var request = new RestRequest("/", Method.GET);
+            var response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
